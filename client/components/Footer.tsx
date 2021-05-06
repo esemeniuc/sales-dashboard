41f397a33bca8b3af345d33aec443d0195bf6063
Eric Semeniuc <3838856+esemeniuc@users.noreply.github.com>
2021-05-06 13:40:28 -0700

    remove mui

--- client/components/Footer.tsx
+++ client/components/Footer.tsx
@@ -1,39 +1,25 @@
 import React from 'react';
-import Typography from '@material-ui/core/Typography';
-import Link from '@material-ui/core/Link';
-import Container from '@material-ui/core/Container';
-import {makeStyles} from "@material-ui/core/styles";
 
-const useStyles = makeStyles((theme) => ({
-    footer: {
-        borderTop: `1px solid ${theme.palette.divider}`,
-        marginTop: theme.spacing(8),
-        paddingTop: theme.spacing(3),
-        paddingBottom: theme.spacing(3),
-        [theme.breakpoints.up('sm')]: {
-            paddingTop: theme.spacing(6),
-            paddingBottom: theme.spacing(6),
-        },
-    },
-}));
 
-export function Footer() {
-    const classes = useStyles();
-    return <Container maxWidth="md" component="footer" className={classes.footer}>
-        <CopyrightComponent/>
-    </Container>
-}
 
-function CopyrightComponent() {
-    return <>
-        <Typography variant="body1" color="textSecondary" align="center">
-            Made with ♥ in SF
-        </Typography>
-        <Typography variant="body2" color="textSecondary" align="center">
-            {`Copyright © ${new Date().getFullYear()} `}
-            <Link color="inherit" href="https://romeano.com">
-                romeano
-            </Link>
-        </Typography>
-    </>
-}
+
+// export function Footer() {
+//     const classes = useStyles();
+//     return <Container maxWidth="md" component="footer" className={classes.footer}>
+//         <CopyrightComponent/>
+//     </Container>
+// }
+//
+// function CopyrightComponent() {
+//     return <>
+//         <Typography variant="body1" color="textSecondary" align="center">
+//             Made with ♥ in SF
+//         </Typography>
+//         <Typography variant="body2" color="textSecondary" align="center">
+//             {`Copyright © ${new Date().getFullYear()} `}
+//             <Link color="inherit" href="https://romeano.com">
+//                 romeano
+//             </Link>
+//         </Typography>
+//     </>
+// }
--- client/components/Link.tsx
+++ client/components/Link.tsx
@@ -1,9 +1,9 @@
 /* eslint-disable jsx-a11y/anchor-has-content */
 import * as React from 'react';
-import clsx from 'clsx';
+// import clsx from 'clsx';
 import { useRouter } from 'next/router';
 import NextLink, { LinkProps as NextLinkProps } from 'next/link';
-import MuiLink, { LinkProps as MuiLinkProps } from '@material-ui/core/Link';
+// import MuiLink, { LinkProps as MuiLinkProps } from '@material-ui/core/Link';
 
 type NextComposedProps = Omit<React.AnchorHTMLAttributes<HTMLAnchorElement>, 'href'> &
     NextLinkProps;
@@ -31,42 +31,42 @@ interface LinkPropsBase {
     innerRef?: React.Ref<HTMLAnchorElement>;
     naked?: boolean;
 }
-
-export type LinkProps = LinkPropsBase & NextComposedProps & Omit<MuiLinkProps, 'href'>;
-
-// A styled version of the Next.js Link component:
-// https://nextjs.org/docs/#with-link
-function Link(props: LinkProps) {
-    const {
-        href,
-        activeClassName = 'active',
-        className: classNameProps,
-        innerRef,
-        naked,
-        ...other
-    } = props;
-
-    const router = useRouter();
-    const pathname = typeof href === 'string' ? href : href.pathname;
-    const className = clsx(classNameProps, {
-        [activeClassName]: router.pathname === pathname && activeClassName,
-    });
-
-    if (naked) {
-        return <NextComposed className={className} ref={innerRef} href={href} {...other} />;
-    }
-
-    return (
-        <MuiLink
-            component={NextComposed}
-            className={className}
-            ref={innerRef}
-            href={href as string}
-            {...other}
-        />
-    );
-}
-
-export default React.forwardRef<HTMLAnchorElement, LinkProps>((props, ref) => (
-    <Link {...props} innerRef={ref} />
-));
+//
+// export type LinkProps = LinkPropsBase & NextComposedProps & Omit<MuiLinkProps, 'href'>;
+//
+// // A styled version of the Next.js Link component:
+// // https://nextjs.org/docs/#with-link
+// function Link(props: LinkProps) {
+//     const {
+//         href,
+//         activeClassName = 'active',
+//         className: classNameProps,
+//         innerRef,
+//         naked,
+//         ...other
+//     } = props;
+//
+//     const router = useRouter();
+//     const pathname = typeof href === 'string' ? href : href.pathname;
+//     const className = clsx(classNameProps, {
+//         [activeClassName]: router.pathname === pathname && activeClassName,
+//     });
+//
+//     if (naked) {
+//         return <NextComposed className={className} ref={innerRef} href={href} {...other} />;
+//     }
+//
+//     return (
+//         <MuiLink
+//             component={NextComposed}
+//             className={className}
+//             ref={innerRef}
+//             href={href as string}
+//             {...other}
+//         />
+//     );
+// }
+//
+// export default React.forwardRef<HTMLAnchorElement, LinkProps>((props, ref) => (
+//     <Link {...props} innerRef={ref} />
+// ));
