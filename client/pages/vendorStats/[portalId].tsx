0392b9052044bf2cbf7f0eb851df42debad110a6
Eric Semeniuc <3838856+esemeniuc@users.noreply.github.com>
2021-06-05 12:54:03 -0700

    closer styling for engagement

--- client/pages/vendorStats/[portalId].tsx
+++ client/pages/vendorStats/[portalId].tsx
@@ -29,20 +29,24 @@ export function OpportunityEngagementDemo() {
 
     const maxClickCount = data.reduce((prevMax, currVal) => Math.max(prevMax, currVal.eventCount), 0);
     return <Card borderless>
-        <CardHeader>Opportunity Engagement</CardHeader>
-        <h2 className="text-sm leading-6 text-gray-700">(Clicks)</h2>
-        {
-            data.map(company => {
-                return <div>
-                    {company.customer}
-                    <div className="flex align-middle gap-1">
-                        <div className="h-10 w-full rounded ring-1 ring-inset ring-black ring-opacity-0 bg-green-500"
-                             style={{height: 20, width: `${Math.ceil(100 * company.eventCount / maxClickCount)}%`}}/>
-                        {company.eventCount}
-                    </div>
-                </div>;
-            })
-        }
+        <div className="flex items-end gap-x-2">
+            <CardHeader>Opportunity Engagement</CardHeader>
+            <div className="text-sm text-gray-700">(clicks)</div>
+        </div>
+        <div className="py-3">
+            {
+                data.map(company =>
+                    <>
+                        <span className="text-sm">{company.customer}</span>
+                        <div className="flex items-center gap-1">
+                            <div className="h-8 w-full rounded ring-1 ring-inset ring-black ring-opacity-0 bg-green-500"
+                                 style={{width: `${Math.ceil(100 * company.eventCount / maxClickCount)}%`}}/>
+                            <span className="font-bold text-sm">{company.eventCount}</span>
+                        </div>
+                    </>
+                )
+            }
+        </div>
     </Card>;
 }
 
