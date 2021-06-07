061eec4c480ee789643b56baa8b3fb28760a41a8
Eric Semeniuc <3838856+esemeniuc@users.noreply.github.com>
2021-06-07 09:37:32 -0700

    add header

--- client/pages/vendorStats/ActivePortals.tsx
+++ client/pages/vendorStats/ActivePortals.tsx
@@ -8,6 +8,7 @@ import {getInitialsOfName} from "../../util/text";
 import {CheckIcon} from "@heroicons/react/solid";
 import {BACKEND_ENDPOINT} from "../../config";
 import Link from "../../components/generic/Link";
+import {Card, CardHeader} from "../../components/generic/Card";
 
 const portals: Array<{
     customerName: string,
@@ -149,110 +150,115 @@ function StakeholderClickCircles(props: { data: Array<Stakeholder & { eventCount
 
 export function ActivePortalsDemo() {
     return (
-        <div className="flex flex-col">
-            <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-                <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-                    <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-                        <table className="min-w-full divide-y divide-gray-200">
-                            <thead className="bg-gray-50">
-                            <tr>
-                                <th
-                                    scope="col"
-                                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                                >
-                                    Opportunity
-                                </th>
-                                <th
-                                    scope="col"
-                                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                                >
-                                    Primary Contact
-                                </th>
-                                <th
-                                    scope="col"
-                                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                                >
-                                    Stakeholder Clicks
-                                </th>
-                                <th
-                                    scope="col"
-                                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                                >
-                                    Document Opens
-                                </th>
-                                <th scope="col" className="relative px-6 py-3">
-                                    <span className="sr-only">Edit</span>
-                                </th>
-                            </tr>
-                            </thead>
-                            <tbody className="bg-white divide-y divide-gray-200">
-                            {portals.map((portal, idx) => (
-                                <tr key={idx} className="divide-x">
-                                    <td className="px-6 py-4 whitespace-nowrap">
-                                        <div className="flex items-center">
-                                            <div className="flex flex-col gap-y-1">
-                                                <div className="text-lg font-medium text-gray-900">
-                                                    {portal.customerName}
+        <Card>
+            <CardHeader>
+                Active Portals
+            </CardHeader>
+            <div className="flex flex-col pt-4">
+                <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                    <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                        <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                            <table className="min-w-full divide-y divide-gray-200">
+                                <thead className="bg-gray-50">
+                                <tr>
+                                    <th
+                                        scope="col"
+                                        className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                                    >
+                                        Opportunity
+                                    </th>
+                                    <th
+                                        scope="col"
+                                        className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                                    >
+                                        Primary Contact
+                                    </th>
+                                    <th
+                                        scope="col"
+                                        className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                                    >
+                                        Stakeholder Clicks
+                                    </th>
+                                    <th
+                                        scope="col"
+                                        className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                                    >
+                                        Document Opens
+                                    </th>
+                                    <th scope="col" className="relative px-6 py-3">
+                                        <span className="sr-only">Edit</span>
+                                    </th>
+                                </tr>
+                                </thead>
+                                <tbody className="bg-white divide-y divide-gray-200">
+                                {portals.map((portal, idx) => (
+                                    <tr key={idx} className="divide-x">
+                                        <td className="px-6 py-4 whitespace-nowrap">
+                                            <div className="flex items-center">
+                                                <div className="flex flex-col gap-y-1">
+                                                    <div className="text-lg font-medium text-gray-900">
+                                                        {portal.customerName}
+                                                    </div>
+                                                    <ProgressBullets current={portal.customerCurrentStage}
+                                                                     total={portal.customerNumberOfStages}/>
                                                 </div>
-                                                <ProgressBullets current={portal.customerCurrentStage}
-                                                                 total={portal.customerNumberOfStages}/>
                                             </div>
-                                        </div>
-                                    </td>
-                                    <td className="px-6 py-4 whitespace-nowrap">
-                                        <div className="relative bg-white flex items-center space-x-3">
-                                            <div className="flex-1 min-w-0">
-                                                <p className="font-medium text-gray-900">{portal.vendorContact.name}</p>
-                                                <p className="text-sm truncate">{portal.vendorContact.jobTitle}</p>
-                                            </div>
-                                            <NextLink href={`mailto:${portal.vendorContact.email}`}>
-                                                <div
-                                                    className="w-10 h-10 border-2 flex items-center justify-center border-grey-600 rounded-full ">
-                                                    <MailIcon className="h-4 w-4 text-gray-400"/>
+                                        </td>
+                                        <td className="px-6 py-4 whitespace-nowrap">
+                                            <div className="relative bg-white flex items-center space-x-3">
+                                                <div className="flex-1 min-w-0">
+                                                    <p className="font-medium text-gray-900">{portal.vendorContact.name}</p>
+                                                    <p className="text-sm truncate">{portal.vendorContact.jobTitle}</p>
                                                 </div>
-                                            </NextLink>
-                                        </div>
-                                    </td>
-                                    <td className="px-6 py-4 whitespace-nowrap">
-                                        <div className="flex gap-3">
-                                            <StakeholderClickCircles data={portal.stakeholderEvents}/>
-                                        </div>
-                                    </td>
-                                    <td className="px-6 py-4 whitespace-nowrap">
-                                        <div className="text-sm flex justify-center flex-col gap-y-1">
-                                            {
-                                                portal.documentEvents.slice(0, 3).map(document =>
-                                                    <>
+                                                <NextLink href={`mailto:${portal.vendorContact.email}`}>
+                                                    <div
+                                                        className="w-10 h-10 border-2 flex items-center justify-center border-grey-600 rounded-full ">
+                                                        <MailIcon className="h-4 w-4 text-gray-400"/>
+                                                    </div>
+                                                </NextLink>
+                                            </div>
+                                        </td>
+                                        <td className="px-6 py-4 whitespace-nowrap">
+                                            <div className="flex gap-3">
+                                                <StakeholderClickCircles data={portal.stakeholderEvents}/>
+                                            </div>
+                                        </td>
+                                        <td className="px-6 py-4 whitespace-nowrap">
+                                            <div className="text-sm flex justify-center flex-col gap-y-1">
+                                                {
+                                                    portal.documentEvents.slice(0, 3).map((document, idx) =>
+                                                            <div key={idx}>
                                                     <span className="flex gap-x-1">
                                                         <Link href={`${BACKEND_ENDPOINT}/${document.href}`}>
                                                         {document.body}
                                                     </Link>
                                                    <span className={"text-gray-900"}>{document.eventCount}</span>
                                                     </span>
-                                                    </>
-                                                )
-                                            }
-                                        </div>
-                                    </td>
-                                    <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                                        <NextLink href={`${BACKEND_ENDPOINT}/${portal.portalHref}`}>
-                                            <button
-                                                type="button"
-                                                className="inline-flex items-center px-5 py-3 border shadow-sm text-sm\
+                                                            </div>
+                                                    )
+                                                }
+                                            </div>
+                                        </td>
+                                        <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                                            <NextLink href={`${BACKEND_ENDPOINT}/${portal.portalHref}`}>
+                                                <button
+                                                    type="button"
+                                                    className="inline-flex items-center px-5 py-3 border shadow-sm text-sm\
              leading-4 font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50\
               focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 border-gray-300">
-                                                View
-                                            </button>
-                                        </NextLink>
-                                    </td>
-                                </tr>
-                            ))}
-                            </tbody>
-                        </table>
+                                                    View
+                                                </button>
+                                            </NextLink>
+                                        </td>
+                                    </tr>
+                                ))}
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </Card>
     );
 }
 
