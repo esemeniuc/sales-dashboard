03d066d8e8b0649d132b6bd6ead35ba408ab3f4a
Eric Semeniuc <3838856+esemeniuc@users.noreply.github.com>
2021-06-13 08:03:28 -0700

    fix circles not appearing

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,11 +1,16 @@
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
+    purge: [
+        './pages/**/*.{js,ts,jsx,tsx}',
+        './components/**/*.{js,ts,jsx,tsx}',
+        './util/**/*.{js,ts,jsx,tsx}',
+        './src/**/*.{js,ts,jsx,tsx}'
+    ],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+};
