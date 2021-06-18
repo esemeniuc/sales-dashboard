a28a3912ac8b35983b95581680c43efd6f3048ce
Eric Semeniuc <3838856+esemeniuc@users.noreply.github.com>
2021-06-18 07:59:26 -0700

    db work

--- romeano/app/vendor-stats/queries/getVendorStats.ts
+++ romeano/app/vendor-stats/queries/getVendorStats.ts
@@ -39,55 +39,6 @@ export default resolver.pipe(resolver.zod(GetVendorStats), async ({ id }) => {
 
   if (!portal) throw new NotFoundError()
 
-  const opportunityOverview = {
-    currentRoadmapStage: portal.currentRoadmapStage,
-    stages: portal.roadmapStages.map(stage => ({
-      heading: stage.heading,
-      date: stage.date?.toISOString(),
-      tasks: stage.tasks.map(task => task.task),
-      ctaLink: stage.ctaLinkText && stage.ctaLink ? { body: stage.ctaLinkText, href: stage.ctaLink } : undefined
-    }))
-  }
-
-  const contacts = {
-    contacts: portal.userPortals
-      .filter(userPortal => userPortal.role === Role.AccountExecutive)
-      .map(userPortal =>
-        ({
-          name: `${userPortal.user.firstName} ${userPortal.user.lastName}`,
-          jobTitle: userPortal.user.accountExecutive?.jobTitle,
-          email: userPortal.user.email,
-          photoUrl: userPortal.user.photoUrl ?? "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
-        })
-      )
-  }
-
-
-  const documents = {
-    customer: {
-      name: portal.customerName,
-      documents: portal.documents
-        .filter(x => portal.userPortals.filter(up => up.role === Role.AccountExecutive).map(up => up.userId).includes(x.userId))
-        .map(x => ({
-          id: x.id,
-          title: x.title,
-          href: x.href,
-          isCompleted: x.isCompleted
-        }))
-    },
-    vendor: {
-      name: portal.vendor.name,
-      documents: portal.documents
-        .filter(x => portal.userPortals.filter(up => up.role === Role.Stakeholder).map(up => up.userId).includes(x.userId))
-        .map(x => ({
-          id: x.id,
-          title: x.title,
-          href: x.href,
-          isCompleted: x.isCompleted
-        }))
-    }
-  }
-
   const opportunityEngagement = [
     {
       customer: "Koch",
