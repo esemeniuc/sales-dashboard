513f20d927fd1f8847e013c94ae3bd133d07b9d1
Eric Semeniuc <3838856+esemeniuc@users.noreply.github.com>
2021-07-15 06:59:49 -0700

    update last active

--- romeano/app/core/components/portalDetails/StakeholderEngagementCard.tsx
+++ romeano/app/core/components/portalDetails/StakeholderEngagementCard.tsx
@@ -12,27 +12,30 @@ type StakeholderStats = {
 export function StakeholderEngagementCard(props: { data: StakeholderStats[] }) {
   const maxClickCount = props.data.reduce((prevMax, currVal) => Math.max(prevMax, currVal.eventCount), 0)
   return <Card borderless>
-    <div className="flex items-end gap-x-2">
-      <CardHeader>Stakeholder Engagement</CardHeader>
-      <div className="text-sm text-gray-700">(clicks)</div>
+    <div className="flex items-end justify-between">
+      <div className="flex items-end gap-x-2">
+        <CardHeader>Stakeholder Engagement</CardHeader>
+        <span className="text-sm text-gray-700">(clicks)</span>
+      </div>
+      <span className="text-sm text-gray-700">Last Active</span>
     </div>
-    <div className="py-3 grid gap-x-4 gap-y-2 items-center" style={{ gridTemplateColumns: "auto minmax(0, 1fr) auto" }}>
+    <div className="py-3 grid gap-x-6 gap-y-2 items-center" style={{ gridTemplateColumns: "auto minmax(0, 1fr) auto" }}>
       {
-        props.data.map(stakeholder =>
-          <>
+        props.data.map((stakeholder, idx) =>
+          <React.Fragment key={idx}>
             <div>
               <div className="text-sm font-bold">{stakeholder.stakeholderName}</div>
               <div className="text-sm">{stakeholder.stakeholderJobTitle}</div>
             </div>
-            <div className="flex flex-1 items-center gap-1">
+            <div className="flex flex-1 items-center gap-2">
               <div className="h-8 w-full rounded ring-1 ring-inset ring-black ring-opacity-0 bg-green-500"
                    style={{ width: `${Math.ceil(100 * stakeholder.eventCount / maxClickCount)}%` }} />
               <span className="font-bold text-sm">{stakeholder.eventCount}</span>
             </div>
-            <div
-              className="text-right text-sm text-gray-500">{timeAgo(new Date(stakeholder.lastActive))}
+            <div className="text-right text-sm text-gray-500">
+              {timeAgo(new Date(stakeholder.lastActive))} ago
             </div>
-          </>
+          </React.Fragment>
         )
       }
     </div>
--- romeano/app/core/config.ts
+++ romeano/app/core/config.ts
@@ -1,4 +1,4 @@
-export const PUBLIC_PORT = 3001;
+export const PUBLIC_PORT = 3000;
 
 export const BACKEND_ENDPOINT = process.env.NODE_ENV === 'production' ? "http://dev.romeano.com" : `http://localhost:${PUBLIC_PORT}`;
 export const PUBLIC_ROOT_URL = process.env.NODE_ENV === "development" ? "https://6866ccb5.ngrok.io" : "https://romeano.com";
