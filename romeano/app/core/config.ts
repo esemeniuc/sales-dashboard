ea118946a717df23fa43b1ea67bc30400213c4dc
Eric Semeniuc <3838856+esemeniuc@users.noreply.github.com>
2021-07-22 10:13:18 -0700

    use https

--- romeano/app/core/config.ts
+++ romeano/app/core/config.ts
@@ -1,6 +1,6 @@
 export const PUBLIC_PORT = 3000;
 
-export const BACKEND_ENDPOINT = process.env.NODE_ENV === 'production' ? "http://dev.romeano.com" : `http://localhost:${PUBLIC_PORT}`;
+export const BACKEND_ENDPOINT = process.env.NODE_ENV === 'production' ? "https://dev.romeano.com" : `http://localhost:${PUBLIC_PORT}`;
 export const PUBLIC_ROOT_URL = process.env.NODE_ENV === "development" ? "https://6866ccb5.ngrok.io" : "https://romeano.com";
 export const UPLOAD_DIR = "uploads"; // the /public is not needed
 export const UPLOAD_SIZE_LIMIT = 25 * 1024 * 1024;
