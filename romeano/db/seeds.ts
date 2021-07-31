1a38207478180ef965b64aef87a54f1bd3d6983e
Eric Semeniuc <3838856+esemeniuc@users.noreply.github.com>
2021-07-31 13:47:18 -0700

    update seed

--- romeano/db/seeds.ts
+++ romeano/db/seeds.ts
@@ -15,57 +15,62 @@ const seedCustomerPortal = async () => {
 
   const vendorTeam = await db.vendorTeam.create({
     data: {
-      vendor: { //make vendor
+      vendor: {
+        //make vendor
         create: {
           name: "Mira",
-          logoUrl: "https://images.squarespace-cdn.com/content/v1/59ecb4ff4c0dbfd368993258/1519077349473-M7ADD9VEABMQSHAJB6ZL/ke17ZwdGBToddI8pDm48kEEk35wlJZsUCSxoPFFCfNNZw-zPPgdn4jUwVcJE1ZvWQUxwkmyExglNqGp0IvTJZamWLI2zvYWH8K3-s_4yszcp2ryTI0HqTOaaUohrI8PICXa7_N5J40iYbFYBr4Oop3ePWNkItV5sPMJ0tw-x6KIKMshLAGzx4R3EDFOm1kBS/Mira+Labs+logo.jpg"
-        }
-      }
-    }
+          logoUrl:
+            "https://images.squarespace-cdn.com/content/v1/59ecb4ff4c0dbfd368993258/1519077349473-M7ADD9VEABMQSHAJB6ZL/ke17ZwdGBToddI8pDm48kEEk35wlJZsUCSxoPFFCfNNZw-zPPgdn4jUwVcJE1ZvWQUxwkmyExglNqGp0IvTJZamWLI2zvYWH8K3-s_4yszcp2ryTI0HqTOaaUohrI8PICXa7_N5J40iYbFYBr4Oop3ePWNkItV5sPMJ0tw-x6KIKMshLAGzx4R3EDFOm1kBS/Mira+Labs+logo.jpg",
+        },
+      },
+    },
   })
 
   const aeUser = await db.user.create({
-      data: {
-        firstName: "Greg",
-        lastName: "Miller",
-        email: "greg@mira.com",
-        photoUrl: "https://images.unsplash.com/photo-1499996860823-5214fcc65f8f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1002&q=80",
-        hashedPassword: "", //only works in dev mode!
-        accountExecutive: { //make AE
-          create: {
-            jobTitle: "Account Executive",
-            vendorTeamId: vendorTeam.vendorId
-          }
-        }
+    data: {
+      firstName: "Greg",
+      lastName: "Miller",
+      email: "greg@mira.com",
+      photoUrl:
+        "https://images.unsplash.com/photo-1499996860823-5214fcc65f8f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1002&q=80",
+      hashedPassword: "", //only works in dev mode!
+      accountExecutive: {
+        //make AE
+        create: {
+          jobTitle: "Account Executive",
+          vendorTeamId: vendorTeam.vendorId,
+        },
       },
-      include: {
-        accountExecutive: { include: { vendorTeam: { include: { vendor: true } } } }
-      }
-    }
-  )
+    },
+    include: {
+      accountExecutive: { include: { vendorTeam: { include: { vendor: true } } } },
+    },
+  })
   const aeUser2 = await db.user.create({
-      data: {
-        firstName: "Alexis",
-        lastName: "Linton",
-        email: "alexis@mira.com",
-        photoUrl: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-        accountExecutive: { //make AE
-          create: {
-            jobTitle: "Customer Success Manager",
-            vendorTeamId: vendorTeam.vendorId
-          }
-        }
+    data: {
+      firstName: "Alexis",
+      lastName: "Linton",
+      email: "alexis@mira.com",
+      photoUrl:
+        "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+      accountExecutive: {
+        //make AE
+        create: {
+          jobTitle: "Customer Success Manager",
+          vendorTeamId: vendorTeam.vendorId,
+        },
       },
-      include: {
-        accountExecutive: { include: { vendorTeam: { include: { vendor: true } } } }
-      }
-    }
-  )
+    },
+    include: {
+      accountExecutive: { include: { vendorTeam: { include: { vendor: true } } } },
+    },
+  })
 
   const portal = await db.portal.create({
     data: {
       customerName: "Koch",
-      customerLogoUrl: "https://gray-kwch-prod.cdn.arcpublishing.com/resizer/gLAX07TEGwQfEgBOQ3quD5JAugM=/1200x400/smart/cloudfront-us-east-1.images.arcpublishing.com/gray/IKLFKUHCCJCO3GQSYNXHJOAOSU.JPG",
+      customerLogoUrl:
+        "https://gray-kwch-prod.cdn.arcpublishing.com/resizer/gLAX07TEGwQfEgBOQ3quD5JAugM=/1200x400/smart/cloudfront-us-east-1.images.arcpublishing.com/gray/IKLFKUHCCJCO3GQSYNXHJOAOSU.JPG",
       currentRoadmapStage: 2,
       userPortals: {
         createMany: {
@@ -74,21 +79,22 @@ const seedCustomerPortal = async () => {
               userId: aeUser.id,
               role: Role.AccountExecutive,
               isPrimaryContact: true,
-              isSecondaryContact: false
+              isSecondaryContact: false,
             },
             {
               userId: aeUser2.id,
               role: Role.AccountExecutive,
               isPrimaryContact: false,
-              isSecondaryContact: true
-            }
-          ]
-        }
+              isSecondaryContact: true,
+            },
+          ],
+        },
       },
-      proposalHeading: "Get some headsets into the hands of your operators and conduct remote audits across your sites.",
+      proposalHeading:
+        "Get some headsets into the hands of your operators and conduct remote audits across your sites.",
       proposalSubheading: "2 Prism Headsets + 4 User Licenses",
-      vendorId: vendorTeam.vendorId
-    }
+      vendorId: vendorTeam.vendorId,
+    },
   })
 
   const rawStakeholders = [
@@ -98,7 +104,8 @@ const seedCustomerPortal = async () => {
       jobTitle: "Director of Operations",
       email: "nick@mira.com",
       hasStakeholderApproved: true,
-      photoUrl: "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1234&q=80"
+      photoUrl:
+        "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1234&q=80",
     },
     {
       firstName: "Kristin",
@@ -106,7 +113,8 @@ const seedCustomerPortal = async () => {
       jobTitle: "Head of Technical Services",
       email: "kristin@mira.com",
       hasStakeholderApproved: true,
-      photoUrl: "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80"
+      photoUrl:
+        "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80",
     },
     {
       firstName: "Wally",
@@ -114,7 +122,8 @@ const seedCustomerPortal = async () => {
       jobTitle: "Senior QA Manager",
       email: "wally@mira.com",
       hasStakeholderApproved: true,
-      photoUrl: "https://images.unsplash.com/photo-1554384645-13eab165c24b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1275&q=80"
+      photoUrl:
+        "https://images.unsplash.com/photo-1554384645-13eab165c24b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1275&q=80",
     },
     {
       firstName: "Penelope",
@@ -122,14 +131,25 @@ const seedCustomerPortal = async () => {
       jobTitle: "Plant Manager",
       email: "penelope@mira.com",
       hasStakeholderApproved: null,
-      photoUrl: "https://images.unsplash.com/photo-1593529467220-9d721ceb9a78?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1271&q=80"
-    }
+      photoUrl:
+        "https://images.unsplash.com/photo-1593529467220-9d721ceb9a78?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1271&q=80",
+    },
+    {
+      firstName: "Eric",
+      lastName: "Semeniuc",
+      jobTitle: "Engineer",
+      email: "eric.semeniuc@gmail.com",
+      hasStakeholderApproved: null,
+      photoUrl:
+        "https://images.unsplash.com/photo-1627531937355-be59a1935885?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=80",
+    },
   ]
 
   const stakeholders = []
 
   for (const stakeholder of rawStakeholders) {
-    stakeholders.push(await db.user.create({
+    stakeholders.push(
+      await db.user.create({
         data: {
           firstName: stakeholder.firstName,
           lastName: stakeholder.lastName,
@@ -137,18 +157,18 @@ const seedCustomerPortal = async () => {
           photoUrl: stakeholder.photoUrl,
           stakeholder: {
             create: {
-              jobTitle: stakeholder.jobTitle
-            }
+              jobTitle: stakeholder.jobTitle,
+            },
           },
           userPortals: {
             create: {
               portalId: portal.id,
               role: Role.Stakeholder,
               hasStakeholderApproved: stakeholder.hasStakeholderApproved,
-              isPrimaryContact: stakeholder.firstName === "Kristin"
-            }
-          }
-        }
+              isPrimaryContact: stakeholder.firstName === "Kristin",
+            },
+          },
+        },
       })
     )
   }
@@ -163,23 +183,23 @@ const seedCustomerPortal = async () => {
       date: new Date(2021, 9, 8),
       tasks: { create: { task: "Go over Mira's platform." } },
       ctaLinkText: "Mira's Slide Deck",
-      ctaLink: "https://www.google.com/webhp?client=firefox-b-d"
+      ctaLink: "https://www.google.com/webhp?client=firefox-b-d",
     },
     {
       heading: "AR Headset Demo",
       date: new Date(2021, 10, 11),
       tasks: { create: { task: "Demonstrate a live Mira Connect call from headset." } },
       ctaLinkText: "Join Zoom 📞",
-      ctaLink: "https://www.google.com/webhp?client=firefox-b-d"
+      ctaLink: "https://www.google.com/webhp?client=firefox-b-d",
     },
     {
       heading: "Use-Case Planning Workshop",
-      tasks: { create: { task: "Define problem and primary use-case Mira will be used for." } }
+      tasks: { create: { task: "Define problem and primary use-case Mira will be used for." } },
     },
     {
       heading: "Pilot Package Purchase",
-      tasks: { create: { task: "Quote attached below" } }
-    }
+      tasks: { create: { task: "Quote attached below" } },
+    },
   ]
 
   for (const stage of stages) {
@@ -192,21 +212,21 @@ const seedCustomerPortal = async () => {
         portalId: portal.id,
         description: "Schedule AR Headset Demo Call",
         isCompleted: true,
-        userId: aeUser.id
+        userId: aeUser.id,
       },
       {
         portalId: portal.id,
         description: "Invite IT to next meeting",
         isCompleted: false,
-        userId: aeUser.id
+        userId: aeUser.id,
       },
       {
         portalId: portal.id,
         description: "Send Penelope a revised proposal",
         isCompleted: false,
-        userId: stakeholders[0].id
-      }
-    ]
+        userId: stakeholders[0].id,
+      },
+    ],
   })
 
   const documents = await db.document.createMany({
@@ -216,23 +236,23 @@ const seedCustomerPortal = async () => {
         title: "Security Questionnaire",
         path: "security-questionnaire.txt",
         isCompleted: false,
-        userId: aeUser.id
+        userId: aeUser.id,
       },
       {
         portalId: portal.id,
         title: "Vendor Setup",
         path: "vendor-setup.txt",
         isCompleted: false,
-        userId: aeUser.id
+        userId: aeUser.id,
       },
       {
         portalId: portal.id,
         title: "W-9 Form",
         path: "w9.txt",
         isCompleted: true,
-        userId: stakeholders[0].id
-      }
-    ]
+        userId: stakeholders[0].id,
+      },
+    ],
   })
 
   await db.portal.update({
@@ -244,27 +264,27 @@ const seedCustomerPortal = async () => {
           title: "Mira Proposal",
           path: "proposal.txt",
           isCompleted: false,
-          userId: aeUser.id
-        }
-      }
-    }
+          userId: aeUser.id,
+        },
+      },
+    },
   })
 
   await db.portalImage.createMany({
     data: [
       {
         portalId: portal.id,
-        href: "https://www.aniwaa.com/wp-content/uploads/2018/06/AR-glasses-smartphone-Mira-Prism-side.jpg"
+        href: "https://www.aniwaa.com/wp-content/uploads/2018/06/AR-glasses-smartphone-Mira-Prism-side.jpg",
       },
       {
         portalId: portal.id,
-        href: "https://www.dhresource.com/0x0/f2/albu/g6/M00/D9/44/rBVaR1vhNjmAZBd_AAG1Wfrn4Go755.jpg/top-seller-2018-ar-glasses-mira-prism-ar.jpg"
+        href: "https://www.dhresource.com/0x0/f2/albu/g6/M00/D9/44/rBVaR1vhNjmAZBd_AAG1Wfrn4Go755.jpg/top-seller-2018-ar-glasses-mira-prism-ar.jpg",
       },
       {
         portalId: portal.id,
-        href: "https://www.red-dot.org/index.php?f=37089&token=699949922eb8083e9bb5a3f67081e12da55eecff&eID=tx_solr_image&size=large&usage=hero"
-      }
-    ]
+        href: "https://www.red-dot.org/index.php?f=37089&token=699949922eb8083e9bb5a3f67081e12da55eecff&eID=tx_solr_image&size=large&usage=hero",
+      },
+    ],
   })
 
   const productInfoSections = [
@@ -272,37 +292,39 @@ const seedCustomerPortal = async () => {
       heading: "Product Videos",
       links: [
         {
-          body: "Mira Connect", href: "#"
+          body: "Mira Connect",
+          href: "#",
         },
         {
-          body: "Mira Flow", href: "#"
-        }
-      ]
+          body: "Mira Flow",
+          href: "#",
+        },
+      ],
     },
     {
       heading: "Customer Case Studies",
       links: [
         {
-          body: "Cogentrix Case Study - Remote Audits", href: "#"
+          body: "Cogentrix Case Study - Remote Audits",
+          href: "#",
         },
         {
-          body: "Orica Case Study - Remote Troubleshooting", href: "#"
-        }
-      ]
+          body: "Orica Case Study - Remote Troubleshooting",
+          href: "#",
+        },
+      ],
     },
     {
       heading: "Misc",
-      links: [
-        { body: "Device Technical Spec Sheet", href: "#" }
-      ]
+      links: [{ body: "Device Technical Spec Sheet", href: "#" }],
     },
     {
       heading: "Website",
       links: [
         { body: "Mira Home", href: "#" },
-        { body: "Mira FAQ", href: "#" }
-      ]
-    }
+        { body: "Mira FAQ", href: "#" },
+      ],
+    },
   ]
 
   for (const section of productInfoSections) {
@@ -312,14 +334,13 @@ const seedCustomerPortal = async () => {
         heading: section.heading,
         links: {
           createMany: {
-            data: section.links.map(linkElem =>
-              ({
-                linkText: linkElem.body,
-                link: linkElem.href
-              }))
-          }
-        }
-      }
+            data: section.links.map((linkElem) => ({
+              linkText: linkElem.body,
+              link: linkElem.href,
+            })),
+          },
+        },
+      },
     })
   }
 
@@ -328,14 +349,14 @@ const seedCustomerPortal = async () => {
       {
         portalId: portal.id,
         userId: stakeholders[2].id,
-        message: "I wonder how difficult it is to learn how to use the headset"
+        message: "I wonder how difficult it is to learn how to use the headset",
       },
       {
         portalId: portal.id,
         userId: stakeholders[3].id,
-        message: "Let's ask during our demo call on Wed"
-      }
-    ]
+        message: "Let's ask during our demo call on Wed",
+      },
+    ],
   })
 
   // await db.magicLink.createMany({
@@ -367,29 +388,30 @@ const seedCustomerPortal = async () => {
 
 async function seedPortalDetails() {
   const aeUser = await db.user.create({
-      data: {
-        firstName: "Julia",
-        lastName: "Lin",
-        email: "julia@mira.com",
-        photoUrl: "https://images.unsplash.com/photo-1499996860823-5214fcc65f8f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1002&q=80",
-        accountExecutive: { //make AE
-          create: {
-            jobTitle: "Account Executive",
-            vendorTeamId: 1
-          }
-        }
+    data: {
+      firstName: "Julia",
+      lastName: "Lin",
+      email: "julia@mira.com",
+      photoUrl:
+        "https://images.unsplash.com/photo-1499996860823-5214fcc65f8f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1002&q=80",
+      accountExecutive: {
+        //make AE
+        create: {
+          jobTitle: "Account Executive",
+          vendorTeamId: 1,
+        },
       },
-      include: {
-        accountExecutive: { include: { vendorTeam: { include: { vendor: true } } } }
-      }
-    }
-  )
-
+    },
+    include: {
+      accountExecutive: { include: { vendorTeam: { include: { vendor: true } } } },
+    },
+  })
 
   const portal = await db.portal.create({
     data: {
       customerName: "Raytheon",
-      customerLogoUrl: "https://gray-kwch-prod.cdn.arcpublishing.com/resizer/gLAX07TEGwQfEgBOQ3quD5JAugM=/1200x400/smart/cloudfront-us-east-1.images.arcpublishing.com/gray/IKLFKUHCCJCO3GQSYNXHJOAOSU.JPG",
+      customerLogoUrl:
+        "https://gray-kwch-prod.cdn.arcpublishing.com/resizer/gLAX07TEGwQfEgBOQ3quD5JAugM=/1200x400/smart/cloudfront-us-east-1.images.arcpublishing.com/gray/IKLFKUHCCJCO3GQSYNXHJOAOSU.JPG",
       currentRoadmapStage: 3,
       userPortals: {
         createMany: {
@@ -398,15 +420,16 @@ async function seedPortalDetails() {
               userId: aeUser.id,
               role: Role.AccountExecutive,
               isPrimaryContact: true,
-              isSecondaryContact: false
-            }
-          ]
-        }
+              isSecondaryContact: false,
+            },
+          ],
+        },
       },
-      proposalHeading: "Get some headsets into the hands of your operators and conduct remote audits across your sites.",
+      proposalHeading:
+        "Get some headsets into the hands of your operators and conduct remote audits across your sites.",
       proposalSubheading: "2 Prism Headsets + 4 User Licenses",
-      vendorId: 1
-    }
+      vendorId: 1,
+    },
   })
   const stakeholder = await db.user.create({
     data: {
@@ -415,16 +438,16 @@ async function seedPortalDetails() {
       email: "ali@raytheon.com",
       stakeholder: {
         create: {
-          jobTitle: "Director"
-        }
+          jobTitle: "Director",
+        },
       },
       userPortals: {
         create: {
           role: Role.Stakeholder,
-          portalId: portal.id
-        }
-      }
-    }
+          portalId: portal.id,
+        },
+      },
+    },
   })
 
   // await db.magicLink.createMany({
@@ -449,9 +472,9 @@ async function seedPortalDetails() {
         title: "portal2doc",
         path: "portal2doc.txt",
         isCompleted: false,
-        userId: aeUser.id
-      }
-    ]
+        userId: aeUser.id,
+      },
+    ],
   })
 
   await db.portal.update({
@@ -463,10 +486,10 @@ async function seedPortalDetails() {
           title: "Mira Proposal",
           path: "proposal2.txt",
           isCompleted: false,
-          userId: aeUser.id
-        }
-      }
-    }
+          userId: aeUser.id,
+        },
+      },
+    },
   })
 }
 
@@ -475,21 +498,19 @@ async function seedEvents() {
   const now = new Date()
   const start = subDays(now, days)
 
-  const events = range(days * 24).map(i =>
-    ({
-      type: EventType.DocumentOpen,
-      url: faker.internet.url(),
-      ip: faker.internet.ip(),
-      userAgent: faker.internet.userAgent(),
-      documentId: faker.datatype.number({ min: 1, max: 3 }),
-      portalId: 1,
-      userId: 4,
-      createdAt: min([addHours(start, i + faker.datatype.number({ min: -10, max: 10 })), now])
-    })
-  )
+  const events = range(days * 24).map((i) => ({
+    type: EventType.DocumentOpen,
+    url: faker.internet.url(),
+    ip: faker.internet.ip(),
+    userAgent: faker.internet.userAgent(),
+    documentId: faker.datatype.number({ min: 1, max: 3 }),
+    portalId: 1,
+    userId: 4,
+    createdAt: min([addHours(start, i + faker.datatype.number({ min: -10, max: 10 })), now]),
+  }))
 
   await db.event.createMany({
-    data: events
+    data: events,
   })
 }
 
