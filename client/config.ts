564a55858474fa4ab99209d5f62e672a08a6e1a6
Eric Semeniuc <3838856+esemeniuc@users.noreply.github.com>
2021-08-23 14:08:00 -0700

    remove gql code

--- client/config.ts
+++
@@ -1,8 +0,0 @@
-import {ApolloClient, InMemoryCache} from "@apollo/client";
-
-export const BACKEND_ENDPOINT = process.env.NODE_ENV === 'production' ? "http://dev.romeano.com" : "http://localhost:8000";
-
-export const APOLLO_CLIENT = new ApolloClient({
-    uri: `${BACKEND_ENDPOINT}/graphql`,
-    cache: new InMemoryCache()
-});
--- client/pages/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import 'tailwindcss/tailwind.css';
-
-export default function Home() {
-    return <>
-        This is a homepage
-    </>;
-}
