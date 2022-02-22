77786ab26f19a25f1aad5609d8f0c0dfafe3b884
Michael Baldwin <mikemmb73@gmail.com>
2022-02-21 22:38:33 -0800

    changing ctx to hopefully allow this to work?

--- romeano/app/auth/mutations/login.ts
+++ romeano/app/auth/mutations/login.ts
@@ -25,14 +25,13 @@ export default resolver.pipe(resolver.zod(Login), async ({ email, password }, ct
   // This throws an error if credentials are invalid
   const user = await authenticateUser(email, password)
   console.log(ctx.session)
-  ctx.session.$authorize()
+
+  await ctx.session.$create({ userId: user.id, role: user.role as Role })
 
   if (ctx.session == undefined) {
     console.log("CTX")
     console.log(ctx)
   }
-
-  await ctx.session.$create({ userId: user.id, role: user.role as Role })
 
   return user
 })
